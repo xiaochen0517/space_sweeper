5f2334669e5f9661142440190eb6f2c570c6497b
mochenya <2827075398@qq.com>
2025-03-03 12:11:07 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from render import render
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def main():
+    render()
+
+
+if __name__ == '__main__':
+    main()
